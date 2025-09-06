b7683478ff36c3ded4c203bcc6579d730554410c
Anthony ROUSSEL <anthony@roussel.dev>
2025-09-07 00:09:58 +0200

    dogpile-cache: init at 1.4.0

---
+++ pkgs/python-modules/dogpile-cache/package.nix
@@ -0,0 +1,61 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchPypi,
+  pythonOlder,
+
+  # build-system
+  setuptools,
+
+  # dependencies
+  decorator,
+  python-binary-memcached,
+  stevedore,
+  typing-extensions,
+
+  # checks
+  mako,
+  pytestCheckHook,
+}:
+
+buildPythonPackage rec {
+  pname = "dogpile-cache";
+  version = "1.4.0";
+  pyproject = true;
+
+  disabled = pythonOlder "3.9";
+
+  src = fetchPypi {
+    pname = "dogpile_cache";
+    inherit version;
+    hash = "sha256-sAqeL0Cc+b9Iwuej4+aNrF+nWROsvxpi+CfIEtNfPQk=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    decorator
+    stevedore
+  ]
+  ++ lib.optionals (pythonOlder "3.11") [
+    typing-extensions
+  ];
+
+  optional-dependencies = {
+    bmemcached = [
+      python-binary-memcached
+    ];
+  };
+
+  nativeCheckInputs = [
+    mako
+    pytestCheckHook
+  ];
+
+  meta = {
+    description = "Caching front-end based on the Dogpile lock";
+    homepage = "https://github.com/sqlalchemy/dogpile.cache";
+    license = lib.licenses.mit;
+    teams = [ lib.teams.openstack ];
+  };
+}

a41dc2ea52dbc93247d3624f129ecc0b95f7145f
Anthony ROUSSEL <anthony@roussel.dev>
2025-09-07 00:49:20 +0200

    oslo-rootwrap: init at 7.7.0

---
+++ pkgs/python-modules/oslo-rootwrap/package.nix
@@ -0,0 +1,75 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchPypi,
+
+  # build-system
+  pbr,
+  setuptools,
+
+  # dependencies
+  coreutils,
+  debtcollector,
+
+  # checks
+  oslotest,
+  stestr,
+}:
+
+buildPythonPackage rec {
+  pname = "oslo-rootwrap";
+  version = "7.7.0";
+  pyproject = true;
+
+  src = fetchPypi {
+    pname = "oslo_rootwrap";
+    inherit version;
+    hash = "sha256-yyZ+PMUdvyX8fa0Nxt2s7zofCg/aarV5N4JKadTSqSs=";
+  };
+
+  postPatch = ''
+    for file in oslo_rootwrap/tests/test_rootwrap.py oslo_rootwrap/filters.py oslo_rootwrap/tests/test_functional.py; do
+      substituteInPlace $file \
+        --replace "/bin/cat" "${coreutils}/bin/cat" \
+        --replace "/bin/echo" "${coreutils}/bin/echo" \
+        --replace "/bin/kill" "${coreutils}/bin/echo"
+    done
+  '';
+
+  build-system = [
+    pbr
+    setuptools
+  ];
+
+  dependencies = [
+    debtcollector
+    pbr
+  ];
+
+  nativeCheckInputs = [ stestr ];
+
+  checkPhase = ''
+    runHook preCheck
+    stestr run -e <(echo "
+      # Requires networking
+      oslo_rootwrap.tests.test_rootwrap.RootwrapTestCase.test_ChainingRegExpFilter_match
+      oslo_rootwrap.tests.test_rootwrap.RootwrapTestCase.test_ChainingRegExpFilter_multiple
+      oslo_rootwrap.tests.test_rootwrap.RootwrapTestCase.test_exec_dirs_search
+      oslo_rootwrap.tests.test_rootwrap.RootwrapTestCase.test_KillFilter
+      oslo_rootwrap.tests.test_rootwrap.RootwrapTestCase.test_match_filter_recurses_exec_command_filter_matches
+      oslo_rootwrap.tests.test_rootwrap.RootwrapTestCase.test_RegExpFilter_match
+      # Faulty test
+      oslo_rootwrap.tests.test_functional.RootwrapDaemonTest.test_run_with_later_install_cmd
+    ")
+    runHook postCheck
+  '';
+
+  pythonImportsCheck = [ "oslo_rootwrap" ];
+
+  meta = {
+    description = "Oslo Rootwrap library";
+    homepage = "https://github.com/openstack/oslo.rootwrap";
+    license = lib.licenses.asl20;
+    teams = [ lib.teams.openstack ];
+  };
+}
